ae25cb0b9ede61c94a773ca9dc8828570aa73e18
Raghul D <v-raghuld@microsoft.com>
2023-06-14 15:17:06 +0530

    push youtube clone project

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import { Videos, ChannelCard } from './';
+import { fetchFromApi } from '../utils/fetchFromApi';
+
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([])
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromApi(`channels?part=snippet&id=${id}`)
+      .then(data => setChannelDetail(data?.items[0]));
+
+    fetchFromApi(`search?channelId=${id}&part=snippet&order=date`)
+      .then(data => setVideos(data?.items))
+  }, [id])
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(37,182,100,1) 0%, rgba(9,121,50,1) 45%, rgba(0,245,255,1) 100%)',
+          zIndex: 10,
+          height: '300px'
+        }} />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  )
+}
+
+export default ChannelDetail
